148aa9a4425b7083092eb4a374d5ab20898b1493
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-09-01 19:05:10 -0500

    Correct enum switch syntax

--- StructorVerba/Pēnsōrēs/Adiectīva/PēnsorAdiectīvīs.cs
+++ StructorVerba/Pēnsōrēs/Adiectīva/PēnsorAdiectīvīs.cs
@@ -12,14 +12,14 @@ namespace Pēnsōrēs.Adiectiva
     private static readonly Func<ĪnflexorEffectusĀctibus.Versiō, Tabula?> Tabulātor =
               versiō => versiō switch
             {
-              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia ||
-              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Cum_Litterā_Ē ||
-              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Sine_Litterā_Ē ||
-              Versiō.Prōnōminālis || Versiō.Prōnōminālis_Varius
+              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia or
+              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Cum_Litterā_E or
+              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Sine_Litterā_E or
+              Versiō.Prōnōminālis or Versiō.Prōnōminālis_Varius
                         => Tabula.Adiectīva_Aut_Prīma_Aut_Secunda_Aut_Tertia,
-              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda ||
-              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda_Cum_Genitīvō_Variō ||
-              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda_Cum_Ablātīvō_Variō ||
+              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda or
+              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda_Cum_Genitīvō_Variō or
+              Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda_Cum_Ablātīvō_Variō or
               Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda_Cum_Genitīvō_Ablātīvōque_Variō
                         => Tabula.Adiectīva_Aut_Tertia_Aut_Prīma_Aut_Secunda,
               _ => null
@@ -29,8 +29,8 @@ namespace Pēnsōrēs.Adiectiva
               versiō => versiō switch
             {
               Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia => null,
-              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Cum_Litterā_Ē => null,
-              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Sine_Litterā_Ē => null,
+              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Cum_Litterā_E => null,
+              Versiō.Aut_Prīma_Aut_Secunda_Aut_Tertia_Sine_Litterā_E => null,
               Versiō.Prōnōminālis => null,
               Versiō.Prōnōminālis_Varius => null,
               Versiō.Aut_Tertia_Aut_Prīma_Aut_Secunda => null,
