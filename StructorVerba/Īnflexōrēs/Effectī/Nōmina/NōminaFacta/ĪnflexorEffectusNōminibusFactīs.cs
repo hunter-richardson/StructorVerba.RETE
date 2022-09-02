55195c6f7baa90c3b62ab64d7844214408f72767
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-09-02 00:02:50 -0500

    Add known values to abstract constructor and method

--- StructorVerba/Īnflexōrēs/Effectī/Nōmina/NōminaFacta/ĪnflexorEffectusNōminibusFactīs.cs
+++ StructorVerba/Īnflexōrēs/Effectī/Nōmina/NōminaFacta/ĪnflexorEffectusNōminibusFactīs.cs
@@ -34,15 +34,31 @@ namespace Īnfexōrēs.Effectī.Nōmina.NōminaFacta
 
     protected ĪnflexorEffectusNōminibusFactīs(in Versiō versiō, in Lazy<Nūntius<ĪnflexorEffectusNōminibusFactīs>> nūntius,
                                               in Func<Īnflectendum.NōmenFactum, Enum[], string> rādīcātor)
-                                                 : base(versiō, nūntius, nameof(Īnflectendum.NōmenFactum.Īnfīnītum), rādīcātor,
-                                                        Ūtilitātēs.Colligō(Factum.Īnfīnītīvum.SingleItemSet(),
-                                                        Ūtilitātēs.Combīnō(Factum.Gerundātīvum.SingleItemSet(),
-                                                                           new SortedSet<Casus>() { Casus.Genitīvus, Casus.Datīvus, Casus.Accūsātīvus, Casus.Ablātīvus }),
-                                                        Ūtilitātēs.Combīnō(Factum.Supīnum.SingleItemSet(),
-                                                                           new SortedSet<Casus>() { Casus.Accūsātīvus, Casus.Ablātīvus }))) { }
+                                                 : base(versiō, nūntius, nameof(Īnflectendum.NōmenFactum.Īnfīnītum),
+                                                        (nōmen, illa) =>
+                                                        {
+                                                          const Factum factum = Facta.Iactor.Invoke((from illud in illa
+                                                                                                     where illud is Factum
+                                                                                                     select illud).First());
+                                                          const Casus casus = Casūs.Iactor.Invoke((from illud in illa
+                                                                                                   where illud is Casus
+                                                                                                   select illud).First());
+                                                          return Factum.Supīnum.Equals(factum)
+                                                                               .Choose(nōmen.Supīnum.Chop(2), nōmen.Īnfīnītīvum.Chop(3));
+                                                        }, Ūtilitātēs.Colligō(Factum.Īnfīnītīvum.SingleItemSet(),
+                                                           Ūtilitātēs.Combīnō(Factum.Gerundātīvum.SingleItemSet(),
+                                                                              new SortedSet<Casus>() { Casus.Genitīvus, Casus.Datīvus, Casus.Accūsātīvus, Casus.Ablātīvus }),
+                                                           Ūtilitātēs.Combīnō(Factum.Supīnum.SingleItemSet(),
+                                                                              new SortedSet<Casus>() { Casus.Accūsātīvus, Casus.Ablātīvus }))) { }
     protected abstract string Īnfīnītīvum();
     protected abstract string? Gerundīvum(in Casus casus);
-    protected abstract string? Supīnum(in Casus casus);
+    protected sealed string? Supīnum(in Casus casus)
+                 => casus switch
+                    {
+                      Casus.Accusātīvus => "um",
+                      Casus.Ablātīvus => "ū"
+                    };
+
     public sealed string? Suffixum(in Enum[] illa)
     {
       const Factum factum = Facta.Iactor.Invoke((from illud in illa
