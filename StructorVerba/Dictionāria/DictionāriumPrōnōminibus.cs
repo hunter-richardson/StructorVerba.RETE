e4020c2e5e027ad467d590dd74fdfd51aa7a152c
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-09-09 17:00:07 -0500

    Add pregenerated table to pronoun dictionary

--- StructorVerba/Dictionāria/DictionāriumPrōnōminibus.cs
+++ StructorVerba/Dictionāria/DictionāriumPrōnōminibus.cs
@@ -14,6 +14,16 @@ namespace Dictionāria
   {
     public static readonly Lazy<DictionāriumPrōnōminibus> Faciendum = new Lazy(() => Instance);
 
+    public readonly Func<IEnumerable<Enum>[]> Praegenerātor
+        = () => Ūtilitātēs.Seriēs(Ūtilitātēs.Combīnō(Genus.GetValues().Except(Genus.Nūllum).ToHasSet(),
+                                                     new HashSet<Casus>() { Casus.Nōminātīvus, Casus.Genitīvus, Casus.Accūsātīvus, Casus.Vocātīvus },
+                                                     Numerālis.GetValues().Except(Numerālis.Nūllus).ToHashSet()),
+                                  Ūtilitātēs.Combīnō(Genus.GetValues().Except(Genus.Nūllum).ToHasSet(),
+                                                     new HashSet<Casus>() { Casus.Datīvus, Casus.Ablātīvus, Casus.Locātīvus, Casus.Inatrumentālis },
+                                                     Numerālis.Singulāris.SingleItemSet(),
+                                  Ūtilitātēs.Combīnō(new HashSet<Casus>() { Casus.Datīvus, Casus.Ablātīvus, Casus.Locātīvus, Casus.Inatrumentālis },
+                                                     Numerālis.Plūrālis.SingleItemSet());
+
     protected readonly Lazy<ĪnflexorIncertus> Sē = ĪnflexorVerbīSē.Faciendum;
 
     private DictionāriumPrōnōminibus()
