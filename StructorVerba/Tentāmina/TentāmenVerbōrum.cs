537259f1432470391e350c46e56586b8e775cdad
Hunter Richardson <hunter-richardson@users.noreply.github.com>
2022-09-24 22:28:26 +0000

    Remove empty line

--- StructorVerba/Tentāmina/TentāmenVerbōrum.cs
+++ StructorVerba/Tentāmina/TentāmenVerbōrum.cs
@@ -24,46 +24,45 @@ namespace Tentāmina
                         await TentāmenReī.AequāturAsync(Locūtiō.Length, Verba.Count(), Error);
                       };
 
-  private readonly Task<Action> Ultimum
-              = async () =>
-                      {
-                        Verba.ForEach(verbum => await Scrīptor.Additor.Invoke(verbum));
-                        await TentāmenReī.AequāturAsync(Locūtiō, Scrīptor.ToString(), Error);
-                      };
-
-  private readonly Task<Action<Verbum?, string>> Ullum
-              = async (verbum, prōductum) =>
-                      {
-                        const string error = $"Prōducta {Nōmen} relica'st prōductiō verbī {prōductum}";
-                        const TentāmenVerbī tentāmen = new TentāmenVerbī(verbum);
-                        await tentāmen.ExsistatAsync(error);
-                        await tentāmen.AequāturAsync(prōductum, error);
-                      };
-
-  public readonly Task<Func<string>> Tentātor
-              = async () =>
-                      {
-                        await Prīmum.Invoke();
-                        const string[] scrīpta = Locūtiō.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                        for (int i = 0; i < Verba.Count(); i++)
+    private readonly Task<Action> Ultimum
+                = async () =>
                         {
-                          await Ullum.Invoke(Verba.ElementAt(i), scrīpta[i].ToLowerInvariant());
-                        }
-                        await Ultimum.Invoke();
-                        return $"Prōducta {Nōmen}: {Verba}";
+                          Verba.ForEach(verbum => await Scrīptor.Additor.Invoke(verbum));
+                          await TentāmenReī.AequāturAsync(Locūtiō, Scrīptor.ToString(), Error);
+                        };
 
-                      };
+    private readonly Task<Action<Verbum?, string>> Ullum
+                = async (verbum, prōductum) =>
+                        {
+                          const string error = $"Prōducta {Nōmen} relica'st prōductiō verbī {prōductum}";
+                          const TentāmenVerbī tentāmen = new TentāmenVerbī(verbum);
+                          await tentāmen.ExsistatAsync(error);
+                          await tentāmen.AequāturAsync(prōductum, error);
+                        };
 
-  private readonly Enumerable<Verbum?>? Verba { get; }
-  private readonly string Locūtiō { get; }
-  private readonly string Nōmen { get; }
-  private readonly string Error => $"Verba prōducta {Nōmen} exspectātiōne eius differt";
-  private TentāmenVerbōrum(in Enumerable<Verbum?>? verba,
-                           in string locūtiō, in string nōmen = string.Empty)
-  {
-    Verba = verba;
-    Locūtiō = locūtiō;
-    Nōmen = nōmen;
+    public readonly Task<Func<string>> Tentātor
+                = async () =>
+                        {
+                          await Prīmum.Invoke();
+                          const string[] scrīpta = Locūtiō.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                          for (int i = 0; i < Verba.Count(); i++)
+                          {
+                            await Ullum.Invoke(Verba.ElementAt(i), scrīpta[i].ToLowerInvariant());
+                          }
+                          await Ultimum.Invoke();
+                          return $"Prōducta {Nōmen}: {Verba}";
+
+                        };
+    private readonly Enumerable<Verbum?>? Verba { get; }
+    private readonly string Locūtiō { get; }
+    private readonly string Nōmen { get; }
+    private readonly string Error => $"Verba prōducta {Nōmen} exspectātiōne eius differt";
+    private TentāmenVerbōrum(in Enumerable<Verbum?>? verba,
+                            in string locūtiō, in string nōmen = string.Empty)
+    {
+      Verba = verba;
+      Locūtiō = locūtiō;
+      Nōmen = nōmen;
+    }
   }
-}
 }
